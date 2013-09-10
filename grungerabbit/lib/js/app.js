6815f3db0b9d5ebe3eceb93626d897632df1db37
Cheryl Wu <grungerabbit@gmail.com>
2013-09-10 00:51:00 -0400

    more responsive, media queries

--- grungerabbit/lib/js/app.js
+++ grungerabbit/lib/js/app.js
@@ -1,7 +1,13 @@
 $(".title, .name").lettering();
 var c = "";
 $(".title span").each(function() {
-
+	
 	
 	$(this).append("<em>"+$(this).text()+"</em>");
+	
+	
 });
+
+if ($("body").width() < 640) {
+	$(".title span:nth-child(6)").after("<br> -");
+}
